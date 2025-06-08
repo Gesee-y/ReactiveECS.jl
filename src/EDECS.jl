788b0f1b504569ce83288ab7b6c3d7f91865f91c
Talom Laël <gesee37@gmail.com>
2025-06-08 12:57:22 +0100

    Added bitset for archetypes

--- src/EDECS.jl
+++ src/EDECS.jl
@@ -2,6 +2,10 @@
 
 module EDECS
 
+################################################## Entity component system ################################################
+
+#= We need the entity =#
+
 """
     abstract type AbstractEntity
 
@@ -25,22 +29,28 @@ abstract type AbstractSystem end
 
 mutable struct Entity <: AbstractEntity
 	const id::UInt
+	valid::Bool
+	archetype::UInt128
 	components::NamedTuple
 
 	## Constructors
-	Entity(id::Integer;kwargs...) = new(id, NamedTuple(kwargs))
+	Entity(id::Integer,valid=false;kwargs...) = begin
+	    components = NamedTuple(kwargs)
+	    archetype = get_bits(values(components))
+	    new(id, valid, archetype, components)
+	end
 end
 
 # TODO : Search better data structures to keep data ain the ECS manager, it may improve performances
 mutable struct ECSManager{T}
 	entities::Dict{UInt, T}
-	groups::Dict{Tuple, Vector{T}} # We prefer a vector to a dict, it's easier to parallelise
-	systems::Dict{Tuple, Vector{AbstractSystem}}
+	groups::Dict{UInt128, Vector{WeakRef}} # We prefer a vector to a dict, it's easier to parallelise
+	systems::Dict{UInt128, Vector{AbstractSystem}}
 	
 	ECSManager{T}() where T <: AbstractEntity = new{T}(
 		Dict{UInt,T}(), 
-		Dict{Tuple, Vector{UInt}}(),
-		Dict{Tuple, Vector{AbstractSystem}}()
+		Dict{UInt128, Vector{WeakRef}}(),
+		Dict{UInt128, Vector{AbstractSystem}}()
 		)
 end
 
@@ -52,10 +62,11 @@ This add an entity to the ECS and sort it in the existing archetype
 """
 function add_entity!(ecs::ECSManager{T}, entity::T) where T <: AbstractEntity
 	ecs.entities[entity.id] = entity
+	entity.valid = true
 	
 	for archetype in keys(ecs.groups)
 		if _match_archetype(entity, archetype)
-			push!(ecs.groups[archetype], entity)
+			push!(ecs.groups[archetype], WeakRef(entity))
 		end
 	end
 
@@ -68,7 +79,7 @@ This remove an entity to the ECS and remove it in the existing archetype where h
 """
 function remove_entity!(ecs::ECSManager, entity::T) where T <: AbstractEntity
 	delete!(ecs.entities, entity.id)
-	_remove_entity_in_groups(ecs, entity)
+	entity.valid = false
 end
 
 function attach_component!(ecs::ECSManager{T}, entity::T, component::AbstractComponent) where T <: AbstractEntity
@@ -82,7 +93,7 @@ function attach_component!(ecs::ECSManager{T}, entity::T, component::AbstractCom
 
 		for archetype in keys(ecs.groups)
 			if _match_archetype(entity,archetype) && !(entity.id in ecs.groups[archetype])
-				push!(ecs.groups[archetype], entity)
+				push!(ecs.groups[archetype], WeakRef(entit)y)
 			end
 		end
 		
@@ -98,17 +109,43 @@ function detach_component!(ecs::ECSManager{T}, entity::T, component::AbstractCom
 	end
 end
 
+"""
+    validate(ref::WeakRef, i::Int)
+
+This function should be used inside the run! function of a system, it will check the validity of an entity and return it
+"""
+function validate(ref::WeakRef, i::Int)
+	entities = ref.value
+	entity = entities[i]
+	if isnothing(entity) || !(entity.value.valid)
+		lck = ReentrantLock()
+		
+		# We ensure that this sytem is the only one suppressing invalid data
+		lock(lck)
+
+		# We do a swap deletion
+		tmp = pop!(entities)
+		entities[i] = tmp
+
+		# We unlock the process
+		unlock(lck)
+	end
+
+	return entity.value
+end
+
 ############################################## Systems ##################################################
 
-function subscribe!(ecs::ECSManager{T},system::AbstractSystem, archetype::Tuple) where T
+function subscribe!(ecs::ECSManager{T},system::AbstractSystem, components::Tuple) where T
 	
 	# If the is not system with a subscription to the given archetype
+	archetype = get_bits(components)
 	if !haskey(ecs.groups, archetype)
 		ecs.systems[archetype] = AbstractSystem[system]
 		ecs.groups[archetype] = UInt[]
 		for entity in values(ecs.entities)
 			if _match_archetype(entity, archetype)
-				push!(ecs.groups[archetype], entity)
+				push!(ecs.groups[archetype], WeakRef(entity))
 			end
 		end
 	else
@@ -122,10 +159,17 @@ function run_system!(system::AbstractSystem)
 
 	# We skip the error checking on purpose
 	# Just taking data, extremely low risk of error
-	@async while system.active
-		batch = take!(system.flow.input)
-		run!(system, batch)
-	end
+	errormonitor(@async while system.active
+			batch = take!(system.flow.input)
+			res = run!(system, batch)
+			children = get_children(system)
+			
+			if res != nothing
+				for child in children
+					put!(child.flow.input, res)
+				end
+			end
+		end)
 end
 
 include("flow.jl")
@@ -138,9 +182,16 @@ get_component(entity::AbstractEntity, ::T) where T <: AbstractComponent = get_co
 get_name(::Type{T}) where T <: AbstractComponent = Symbol(T.name.name)
 get_name(::T) where T <: AbstractComponent = get_name(T)
 
+get_type(T::Type) = T
+
+get_bits(c::AbstractComponent) = get_bits(typeof(c))
+get_bits(t::Tuple) = +(get_bits.(t)...)
+get_bits(::Type{T}) where T = 0b0
+
 ############################################# Helpers ####################################################
 
-_match_archetype(entity::AbstractEntity, archetype::Tuple) = return all(c -> haskey(entity.components,c), archetype)
+_match_archetype(entity::AbstractEntity, archetype::Tuple) = _match_archetype(entity, get_bits(archetype)) 
+_match_archetype(entity::AbstractEntity, archetype::Unsigned) = (entity.archetype & archetype) == archetype
 
 function _remove_entity_in_groups(ecs::ECSManager, entity::AbstractEntity)
 	for k in keys(ecs.groups)
@@ -153,5 +204,4 @@ end
 function _delete_tuple(t::NamedTuple, c::Symbol)
 	f = findfirst(x -> x==c, keys(t))
 	return NamedTuple{(keys(t[begin:f-1])...,keys(t[f+1:end])...)}((values(t[begin:f-1])..., values(t[f+1:end])))
-end
-end # Module
+endend # Module
