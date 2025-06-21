078bb4737180a02ace6ffa010d227568f33c8e8c
Talom Laël <gesee37@gmail.com>
2025-06-21 23:45:57 +0100

    Update test.jl

--- test/test.jl
+++ test/test.jl
@@ -1,91 +1,77 @@
-using EDECS
+include("..\\src\\RECS.jl")
 
-# Component definitions
-struct Health <: AbstractComponent
-    hp::Int
+using .RECS
+using BenchmarkTools
+using LoopVectorization
+
+@component Health begin
+	hp::Int
 end
-EDECS.get_bits(::Type{Health})::UInt128 = 0b1
 
-mutable struct TransformComponent <: AbstractComponent
+@component Transform begin
     x::Float32
     y::Float32
 end
-EDECS.get_bits(::Type{TransformComponent})::UInt128 = 0b10
 
-struct PhysicComponent <: AbstractComponent
+@component Physic begin
     velocity::Float32
 end
-EDECS.get_bits(::Type{PhysicComponent})::UInt128 = 0b100
 
-# Naming helper for components
-EDECS.get_name(::TransformComponent) = :Transform
-EDECS.get_name(::PhysicComponent)    = :Physic
+@system PhysicSystem
+@system RenderSystem
 
-# System declarations via macro
-@system(PhysicSystem, Entity)
-@system(PrintSystem, Entity)
-@system(RenderSystem, Entity)
+function RECS.run!(sys::PhysicSystem, data)
+	indices = data.value
 
-# System behavior implementations
-function run!(::PhysicSystem, ref::WeakRef)
-    entities = ref.value # Getting the array of entities for the Weak reference
-    for i in eachindex(entities)
-        entity = validate(ref, i) # We check that the entity is valid
-        t = get_component(entity, TransformComponent)
-        v = get_component(entity, PhysicComponent)
-        t.x += v.velocity
+	transform_data = get_component(sys, :Transform)
+	physic_data = get_component(sys, :Physic)
+
+	x_pos = view(transform_data.x, indices)
+	velo = view(physic_data.velocity, indices)
+
+    for i in eachindex(indices)
+	    x_pos[i] += velo[i]
     end
 
-    return ref
+    return transform_data
 end
 
-function run!(sys::PrintSystem, ref::WeakRef)
-    entities = ref.value
-    for i in eachindex(entities)
-        entity = validate(ref, i)
-	id = entity.id
-	println("Entity: $id")
-    end
+function RECS.run!(::RenderSystem, pos)
+    for i in eachindex(pos)
+		t = pos[i]
+	    println("Rendering entity at position ($(t.x), $(t.y))")
+	end
 end
 
-function run!(::RenderSystem, ref)
-    entities = ref.value
-    for i in eachindex(entities)
-	entity = validate(ref, i)
-        t = get_component(entity, TransformComponent)
-        println("Rendering entity $(entity.id) at position ($(t.x), $(t.y))")
-    end
-end
+ecs = ECSManager()
 
+physic_sys = PhysicSystem()
+render_sys = RenderSystem()
 
-# ECS manager initialization
-ecs = ECSManager{Entity}()
+subscribe!(ecs, physic_sys, (TransformComponent, PhysicComponent))
+listen_to(physic_sys,render_sys)
 
-# Create two entities
-e1 = Entity(1; Health = Health(100), Transform = TransformComponent(1.0,2.0))
-e2 = Entity(2; Health = Health(50), Transform = TransformComponent(-5.0,0.0), Physic = PhysicComponent(1.0))
+e1 = create_entity!(ecs; Health = HealthComponent(100), Transform = TransformComponent(1.0,2.0))
+e2 = create_entity!(ecs; Health = HealthComponent(50), Transform = TransformComponent(-5.0,0.0), Physic = PhysicComponent(1.0))
+e3 = create_entity!(ecs; Health = HealthComponent(50), Transform = TransformComponent(-5.0,0.0), Physic = PhysicComponent(1.0))
 
-add_entity!(ecs, e1)
-add_entity!(ecs, e2)
-
-# System instances
-print_sys   = PrintSystem()
-physic_sys  = PhysicSystem()
-render_sys  = RenderSystem()
-
-# Subscribe to archetypes
-subscribe!(ecs, print_sys,   (:Health, :Transform))
-subscribe!(ecs, physic_sys,  (:Transform, :Physic))
-listen_to(physic_sys, render_sys) # This function tells physic system that he should dispatch the results of his process to his listeners
-
-# Launch systems (asynchronous task)
-run_system!(print_sys)
 run_system!(physic_sys)
 run_system!(render_sys)
 
-# Simulate 3 frames
-for i in 1:3
-    println("FRAME $i")
-    dispatch_data(ecs)
-    yield()
+N = 5
+target = 16 * 10^6
+
+println("STARTING")
+for i in 1:N
+    begin
+        st = time_ns()
+	println("FRAME $i")
+	dispatch_data(ecs)
+	blocker(ecs)
+	dt = (time_ns() - st)
+
+	dt < target && RECS.sleep_ns(target - dt)
+    end
 end
+
+sleep(2)
