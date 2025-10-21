8cf0a5906390038b65427ab237cc1f626c7610ef
Talom Laël <gesee37@gmail.com>
2025-10-21 02:20:35 +0100

    updated benchs

---
+++ benchmark/querying_unoptimized.jl
@@ -0,0 +1,51 @@
+#########################################################################################################################
+####################################################### MANAGER #########################################################
+#########################################################################################################################
+
+include("..\\src\\ReactiveECS.jl")
+using .ReactiveECS
+using BenchmarkTools
+using LoopVectorization
+
+@component Position begin
+    x::Float64
+    y::Float64
+end
+@component Velocity begin
+    dx::Float64
+    dy::Float64
+end
+
+function setup_world(n_entities::Int)
+    world = ECSManager()
+    register_component!(world, Position)
+    register_component!(world, Velocity)
+
+    pos, vel = get_component(world, :Position), get_component(world, :Velocity)
+    request_entity!(world, (;Position= (i -> Position(i, i*2)), Velocity=Velocity(1,1)), n_entities)
+
+    return (pos, vel, @query(world, Position & Velocity))
+end
+
+function benchmark_iteration(n)
+    bench = @benchmarkable begin
+        pos_column, vel_column, query = data
+        @foreachrange query begin
+            @inbounds for i in range
+                pos = pos_column[i]
+                vel = vel_column[i]
+                pos_column[i] = Position(pos.x + vel.dx, pos.y + vel.dy)
+            end
+        end
+    end setup = (data = setup_world($n))
+
+    println("\nBenchmarking with $n entities...")
+    tune!(bench)
+    result = run(bench, seconds=10)
+    println("Mean time per entity: $(time(mean(result)) / n) ns")
+    display(result)
+end
+
+for n in (100, 1_000, 10_000, 100_000)
+    benchmark_iteration(n)
+end
